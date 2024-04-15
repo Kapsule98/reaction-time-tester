a0afa1f9e45e91a969d879a5fd1be22133a242f0
Kapsule98 <kapil9787@gmail.com>
2024-04-15 22:28:26 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction time test</h1>
+  <p>Press the start button and when the red box appears click on it as fast as you can</p>
+  
+  <button class="button" @click="startGame">Start</button>
+  <div @click.self="borderClicked()" class="border"> 
+    <div v-if="showBox" class="box" @click="userTrigger"></div>
+  </div>
+  <div v-if="showResult" class="Result">{{displayScore()}}</div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      showBox: false,
+      startTime: null,
+      endTime: null,
+      showResult: false,
+      gameStarted: false
+    }
+  },
+  methods: {
+    toggleBox() {
+      this.showBox = !this.showBox
+    },
+    startGame() {
+      this.gameStarted = true
+      this.showResult = false
+      setTimeout(()=>{
+        this.toggleBox()
+        this.startTime = Date.now()
+      }, 800 + Math.random()*4000 )
+    },
+    userTrigger() {
+      this.endTime = Date.now()
+      this.showBox = false
+      this.showResult = true
+      this.gameStarted = false
+    },
+    displayScore() {
+      return this.endTime - this.startTime + " ms"
+    },
+    borderClicked() {
+      if(this.gameStarted) {
+        alert("jump start")
+        this.reset()
+      }
+    },
+    reset() {
+      this.showBox = false,
+      this.startTime = null,
+      this.endTime = null,
+      this.showResult = false,
+      this.gameStarted = false
+    }
+    
   }
 }
 </script>
@@ -22,5 +69,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.box {
+  background: blue;
+  width: 20rem;
+  height: 20rem;
+  margin: auto;
+}
+.button {
+  background-color: #04AA6D; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+.border {
+  border: 1px solid #2c3e50;
+  height: 20rem;
+  width: 20rem;
+  margin: auto;
 }
 </style>
